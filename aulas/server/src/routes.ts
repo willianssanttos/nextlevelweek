dfe44a10b7c53ef0d4df58d11a30c38442c72829
willianssantos <willianssantos227@gmail.com>
2020-08-27 18:11:44 -0300

    send project

---
+++ aulas/server/src/routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsControlles';
+
+const routes = express.Router();
+const ClassesControllers = new ClassesController();
+const connectionController = new ConnectionsController();
+
+routes.get('/classes', ClassesControllers.index);
+routes.post('/classes', ClassesControllers.create);
+
+routes.get('/connections', connectionController.index);
+routes.post('/connections', connectionController.create);
+
+
+export default routes;
